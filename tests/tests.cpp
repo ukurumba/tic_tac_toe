c7a695f8f3227c97511e73e1b8e1c252a5a90964
ukurumba <ukurumba@u.rochester.edu>
2017-01-31 12:00:37 -0500

    basic tic tac toe structure

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,11 +2,33 @@
 #include "catch.hpp"
 #include  "../src/classes.cpp"
 
-TEST_CASE( "checking classes" )
+// TEST_CASE("checking user make_move")
+// {
+//   User user1;
+//   Game new_game;
+//   user1.make_move(new_game); //put move as 1
+//     REQUIRE( new_game.board[0]==1 );
+// }
+
+
+TEST_CASE("testing game_over_function")
 {
-  User user1;
-  Game new_game;
-  user1.make_move(new_game); //put move as 1
-    REQUIRE( new_game.board[0]==1 );
+	Game g1,g2,g3;
+	for(int i=0;i<3;i++)
+		g1.board[i] = 1;
+	g2.board[0] =1;
+	g2.board[4] = 1;
+	g2.board[8] = 1;
+	g3.board[2] = 1;
+	g3.board[5] = 1;
+	g3.board[8] = 1;
+	REQUIRE(is_game_over(g1) == true);
+	REQUIRE(is_game_over(g2) == true);
+	REQUIRE(is_game_over(g3) == true);
 }
 
+TEST_CASE("playing game")
+{
+	play_game();
+	REQUIRE(3==3);
+}
