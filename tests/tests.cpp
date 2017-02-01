308c8707245e98b079a9d90417bc9d7d19ab8142
ukurumba <ukurumba@u.rochester.edu>
2017-01-31 20:40:26 -0500

    reworking gameplay

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include  "../src/classes.cpp"
+// #include  "../src/classes.cpp"
+#include "../src/play_game.cpp"
 
 // TEST_CASE("checking user make_move")
 // {
@@ -22,13 +23,13 @@ TEST_CASE("testing game_over_function")
 	g3.board[2] = 1;
 	g3.board[5] = 1;
 	g3.board[8] = 1;
-	REQUIRE(is_game_over(g1) == true);
-	REQUIRE(is_game_over(g2) == true);
-	REQUIRE(is_game_over(g3) == true);
+	REQUIRE(g1.is_game_over() == true);
+	REQUIRE(g2.is_game_over() == true);
+	REQUIRE(g3.is_game_over() == true);
 }
 
-TEST_CASE("playing game")
-{
-	play_game();
-	REQUIRE(3==3);
-}
+// TEST_CASE("playing game")
+// {
+// 	play_game();
+// 	REQUIRE(3==3);
+// }
